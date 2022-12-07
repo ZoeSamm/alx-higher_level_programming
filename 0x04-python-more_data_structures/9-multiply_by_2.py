366672d684797c78b43c91cc2874dc16521c300e
ZoeSamm <zoemultiply@gmail.com>
2022-12-07 15:02:31 -0800

    more data

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    b = dict()
-
-    for item in a_dictionary:
-        b[item] = a_dictionary[item] * 2
-
-    return b
+    return {key: val * 2 for key, val in a_dictionary.items()}
