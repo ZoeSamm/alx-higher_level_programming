016e3ebf965b9db746233149cda916b29bb01cff
ZoeSamm <zoemultiply@gmail.com>
2023-04-04 03:36:24 -0700

    network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+# makes a rerquest to 0.0.0.0:5000/catch_me that gets the message "you got me:".
+
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0"5000/catch_me
