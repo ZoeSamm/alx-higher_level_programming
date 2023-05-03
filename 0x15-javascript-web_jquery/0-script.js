5630e68e21b4fa9f879291341dcb3cd7db739da1
ZoeSamm <beccakem96@gmail.com>
2023-05-03 09:37:40 -0700

    script1

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+<script>
+ // select the header element using document.querySelector
+	const header = document.querySelector('header');
+// update the text color of the header element top red (#FF0000)
+        header.style.color = '#FF0000';
+</script>
